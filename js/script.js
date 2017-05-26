a8daa0ebee1aa45d580a08769bb8960940a8d8a3
RostikMoroziuk <rmorozyukua@gmail.com>
2017-05-26 12:05:22 +0300

    Add multi-local

--- js/script.js
+++ js/script.js
@@ -16,7 +16,16 @@ function addEvent() {
     }
   }
 
-  document.querySelector("form").onsubmit = validateAll;//select first form
+  //select language after loading page
+  addEventListener("load", selectLanguage);
+  //Change language on change radio button
+  var languages = document.querySelectorAll("input[name='lang']");
+  for (i = 0; i < languages.length; i++) {
+    languages[i].addEventListener("change", changeLanguage);
+  }
+
+  document.querySelector("form").onsubmit = validateAll; //select first form
+  document.getElementById("save").addEventListener("click", saveLang);
 }
 
 //Validate age on input
@@ -30,8 +39,7 @@ function ageValidator() {
       alert("You put invalid age data");
       this.value = this.value.replace(findNonDigitPattern, "");
     }
-  }
-  else {
+  } else {
     alert("Age field is empty");
   }
   return true;
@@ -49,8 +57,7 @@ function usernameValidator() {
       this.value = "user_" + this.value; //add user_ at begin of username
       return false;
     }
-  }
-    else {
+  } else {
     alert("Username field is empty");
   }
   return true;
@@ -84,9 +91,9 @@ function dateValidator() {
 
 //Validate all inputs
 function validateAll() {
-  if(!ageValidator.call(document.getElementById("age")) || 
-  !usernameValidator.call(document.getElementById("username")) || 
-  !dateValidator.call(document.getElementById("date"))) {
+  if (!ageValidator.call(document.getElementById("age")) ||
+    !usernameValidator.call(document.getElementById("username")) ||
+    !dateValidator.call(document.getElementById("date"))) {
     return false;
   } else {
     alert("Succes submit. You will be redirect");
@@ -94,4 +101,152 @@ function validateAll() {
   }
 }
 
+var applyLanguage = function (lang) {
+  var i;
+  alert('now language is: ' + lang);
+  showGreeting(lang);
+  //select all tabs and change language
+  var navigationList = document.querySelectorAll("#task-list > li > a")
+  switch (lang) {
+    case "en":
+      navigationList[0].innerText = 'Task "validation"';
+      navigationList[1].innerText = 'Task "parallax"';
+      navigationList[2].innerText = 'Task "os message"';
+      navigationList[3].innerText = 'Task "localization"';
+      navigationList[4].innerText = 'Task "stop & prevent events"';
+      navigationList[5].innerText = 'Task "player navigation"';
+      navigationList[6].innerText = 'Task "list of users"';
+      break;
+    case "ru":
+      navigationList[0].innerText = 'Задание "validation"';
+      navigationList[1].innerText = 'Задание "parallax"';
+      navigationList[2].innerText = 'Задание "os message"';
+      navigationList[3].innerText = 'Задание "localization"';
+      navigationList[4].innerText = 'Задание "stop & prevent events"';
+      navigationList[5].innerText = 'Задание "player navigation"';
+      navigationList[6].innerText = 'Задание "list of users"';
+      break;
+    case "ua":
+      navigationList[0].innerText = 'Завдання "validation"';
+      navigationList[1].innerText = 'Завдання "parallax"';
+      navigationList[2].innerText = 'Завдання "os message"';
+      navigationList[3].innerText = 'Завдання "localization"';
+      navigationList[4].innerText = 'Завдання "stop & prevent events"';
+      navigationList[5].innerText = 'Завдання "player navigation"';
+      navigationList[6].innerText = 'Завдання "list of users"';
+      break;
+  }
+}
+
+//Show greeting msg
+function showGreeting(currentLang) {
+  var i; //counter
+  var langEls = document.getElementsByClassName('lang-' + currentLang);
+  //Hide all msg on old language
+  var visibleEls = document.querySelectorAll(".visible");
+  for (i = 0; i < visibleEls.length; i++) {
+    visibleEls[i].classList.remove("visible");
+  }
+  //Show new msg
+  for (i = 0; i < langEls.length; i++) {
+    var langEl = langEls[i];
+    langEl.classList.add('visible');
+    langEl.innerText += " " + document.getElementById("username").value + ".";
+  }
+}
+
+function selectLanguage() {
+  //show current language
+  var currentLang;
+
+  var getCurrentLanguage = function () {
+    //get current language and transform to common form
+    var curLanguage = navigator.language.toLowerCase();
+    //identify current language
+    if (curLanguage.indexOf("en") >= 0) {
+      curLanguage = "en";
+      applyLanguage(curLanguage);
+    } else if (curLanguage.indexOf("ua") >= 0) {
+      curLanguage = "ua";
+      applyLanguage(curLanguage);
+    } else if (curLanguage.indexOf("ru") >= 0) {
+      curLanguage = "ru";
+      applyLanguage(curLanguage);
+    } else {
+      curLanguage = "en";
+      applyLanguage("Language is undefine. Seted default language (en)");
+    }
+    return curLanguage;
+  }
+
+  function restoreLanguage() {
+    //Try get language from cookie
+    var storeLanguage = getCookie("language");
+    if(!storeLanguage.length) { //if cookie did not found try get from local storage
+      storeLanguage = localStorage.getItem("language");
+    }
+    return storeLanguage;
+  }
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  //If user did not store language data, then get current data of system else data will return in variable
+  currentLang = restoreLanguage(); //return stored language or empty string if data would not be saved
+  console.log("current language " + currentLang);
+  if (!currentLang) {
+    currentLang = getCurrentLanguage();
+  } else {
+    alert("Language restore since last session");
+    applyLanguage(currentLang);
+  }
+  //check radio with current language
+  document.getElementById(currentLang).setAttribute("checked", "checked");
+}
+
+function changeLanguage() {
+  var curLanguage = this.id;
+  applyLanguage(curLanguage);
+}
+
+//Save current language in cookie or localStorage for restoring in future
+function saveLang() {
+  var select = document.querySelector(".select-wrapper .active span") ||
+    document.querySelector(".select-wrapper li span");
+  switch (select.innerText) {
+    case "cookie":
+      if (navigator.cookieEnabled) {
+        //Delete old language cookie
+        document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+        //Set new cookie
+        document.cookie = "language=" + document.querySelector(".check-language[checked]").id;
+        alert("Data saved");
+      } else {
+        alert("You can not save in cookie. Enable cookie please");
+      }
+      break;
+    case "localStorage":
+      //Delete old data
+      localStorage.clear();
+      localStorage.setItem("language", document.querySelector(".check-language[checked]").id);
+      alert("Data saved");
+  }
+}
+
 addEvent();
+//for select field
+$(document).ready(function () {
+  $('select').material_select();
+});
